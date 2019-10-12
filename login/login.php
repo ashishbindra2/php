81c6140cae5bbe5225d4eb71962bbb9346480097
Ashish bindra <ashishbindra2@gmail.com>
2019-10-12 17:07:48 +0530

    login

---
+++ login/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="login.css"><link rel="stylesheet" type="text/css" href="../f/css/all.css">
+</head>
+<body>
+	<div class="loginBox">
+		<img class="user"  src="../avatar/1.png" alt="avatar" width="100px">
+		<h3>Sign In Here </h3>
+		<form>
+			<div class="inputBox">
+				<input type="text" placeholder="Username" name="">
+				<span><i class="fas fa-user-tie"></i> </span>
+			</div>
+			<div class="inputBox">
+				<input type="password" placeholder="Password" name="">
+				<span><i class="fas fa-lock"></i></span>
+			</div>
+			<input type="submit" value="Login" name="">
+		</form>
+		<a href="#"> Froget password</a>
+	</div>
+
+
+</body>
+</html>
