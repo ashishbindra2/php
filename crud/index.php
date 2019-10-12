546cb7609a3ba843cc7b3f81c733a921095217dd
Ashish bindra <ashishbindra2@gmail.com>
2019-10-12 12:34:45 +0530

    phpproject

---
+++ crud/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login Form</title>
+	<link rel="stylesheet" type="text/css" href="signUp.css">
+</head>
+<body>
+	<div class="box">
+		<h2>Contact Us</h2>
+		<form>
+			<div>
+				<input type="text" name="" required="">
+				<label>Name</label>
+			</div>
+			<div>
+				<input type="email" name="" required="">
+				<label>Email</label>	
+			</div>
+			<div>
+				<input type="text" name="" required="">
+				<label>Subject</label>	
+			</div>
+			<div>
+				<textarea required=""></textarea>
+				<label>Message</label>
+			</div>
+			<input type="submit" name="" value="submit">
+		</form>
+	</div>
+</body>
+</html>
